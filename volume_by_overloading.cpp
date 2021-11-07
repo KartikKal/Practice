e06e781745ed8fd10ba048d6b6db33cfb5f3d7f6
KartikKal <93852224+KartikKal@users.noreply.github.com>
2021-11-07 10:18:13 +0530

    Update volume_by_overloading.cpp

--- volume_by_overloading.cpp
+++ volume_by_overloading.cpp
@@ -2,19 +2,23 @@
 #include <iostream>
 int vol(int l)  
 {
+    std::cout<<"volume of cube=";
     return (l*l*l);
 }
 int vol(int l, int b ,int h)
 {
+    std::cout<<"volume of cuboid=";
     return (l*b*h);
 }
 float vol(float r)
 {
+    std::cout<<"volume of circle=";
     return ((4/3)*3.14f*r*r*r);
 }
 int main()
 {
     int a;
-    a = vol(7.0f);
+    a = vol(9);
     std::cout<< a;
+    return 0;
 }
