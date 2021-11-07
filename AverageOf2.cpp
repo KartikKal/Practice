92a99b8c4f1222e8b0225e5768ef58f13f7c7b7c
KartikKal <93852224+KartikKal@users.noreply.github.com>
2021-11-07 09:43:36 +0530

    Create AverageOf2.cpp

---
+++ AverageOf2.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+float avg(int a, int b, int c)
+{
+    return (a+b+c)/2.0f;
+}
+int main()
+{
+    float a;
+    a = avg(4.0f,5);
+    std::cout<< a;\
+    return 0;
+}
